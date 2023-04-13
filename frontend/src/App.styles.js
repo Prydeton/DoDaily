1a4a1be8ba4ea38cd9cf29e1bdc7ac2ac35e1ccc
Prydeton <contact@maxwellreid.tech>
2023-04-13 14:33:59 +1000

    Configure template code

---
+++ frontend/src/App.styles.js
@@ -0,0 +1,5 @@
+import { styled } from 'goober'
+
+export const Container = styled('div')`
+
+`
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import ReactDOM from 'react-dom/client'
+import { setup } from 'goober'
+import { shouldForwardProp } from 'goober/should-forward-prop'
+
 import App from './App'
-import './index.css'
+
+setup(
+  createElement,
+  undefined, undefined,
+  shouldForwardProp(prop => !prop.startsWith('$'))
+)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
